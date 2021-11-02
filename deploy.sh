8c43d9a4ed052158898481c69a9e652975b0722b
JingwenJessica <jingwen.w2010@gmail.com>
2021-11-02 15:45:51 -0400

    rebuilding site Tue Nov  2 15:45:51 EDT 2021

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,11 @@ hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 # Go To Public folder
 cd public
 
-# Add changes to git.
-git add .
+# Add changes to git. 
+# . will stage new files and modifications, without deletions, which would explain why your public/ folder was not being removed.
+#git add .
+# –A will stage all changes
+git add -A
 
 # Commit changes.
 msg="rebuilding site $(date)"
@@ -23,3 +26,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin main
+
+# Come Back up to the Project Root
+cd..
